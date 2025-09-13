a0a8c50d9970a5e7a40cd18303062abe6b6ca91f
hxezu <hailey_apple@icloud.com>
2025-09-13 18:38:34 +0900

    💄 프로젝트 인덱스 제거 후 카드 -> 썸네일로 변경

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,7 +1,10 @@
-import ittaeokImg from "@/assets/images/project1.png";
-import cosmosImg from "@/assets/images/project2.png";
-import popconImg from "@/assets/images/project3.png";
-import ProjectCard from "./project/ProjectCard";
+import ittaeokImg from "@/assets/images/mockup_ittaeok.svg";
+import ittaeokBg from "@/assets/images/background_ittaeok.svg";
+import cosmosImg from "@/assets/images/mockup_cosmos.svg";
+import cosmosBg from "@/assets/images/background_cosmos.png";
+import popconImg from "@/assets/images/mockup_popcon.svg";
+import popconBg from "@/assets/images/background_popcon.svg";
+import ProjectTumbnail from "./project/ProjectThumbnail";
 
 const projects = [
   {
@@ -10,6 +13,7 @@ const projects = [
     description: "효율적인 일정 조율을 위한 통합 스케줄링 플랫폼",
     src: ittaeokImg,
     font: "tttogether",
+    background: ittaeokBg,
   },
   {
     id: "cosmos",
@@ -17,32 +21,34 @@ const projects = [
     description: "우주 콘텐츠 큐레이션 및 커뮤니티 플랫폼",
     src: cosmosImg,
     font: "yapari",
+    background: cosmosBg,
   },
   {
     id: "popcon",
     prjName: "POPcon",
-    description: "해외 팝송 팬들을 위한 음악 추천,\n공연 정보 제공 서비스",
+    description: "해외 팝송 팬들을 위한 음악 추천, 공연 정보 제공 서비스",
     src: popconImg,
     font: "monument",
+    background: popconBg,
   },
 ];
 
 export default function Projects() {
   return (
-    <section className="min-h-screen bg-[color:var(--color-black)] flex flex-col items-center justify-center gap-5">
-      <div className="grid grid-cols-3 gap-20 px-40">
-        {projects.map((project, i) => (
-          <ProjectCard
-            key={i}
+    <section>
+      {projects.map((project, i) => (
+        <div key={i} className="h-screen">
+          <ProjectTumbnail
             index={i + 1}
             id={project.id}
             imgSrc={project.src}
             prjName={project.prjName}
             description={project.description}
             font={project.font}
+            bg={project.background}
           />
-        ))}
-      </div>
+        </div>
+      ))}
     </section>
   );
 }
