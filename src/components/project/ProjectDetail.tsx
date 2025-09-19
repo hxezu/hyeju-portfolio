4eae27408c9b12fe9f8dc0fae865bb460093140a
hxezu <hailey_apple@icloud.com>
2025-09-19 16:33:15 +0900

    style: framer-motion 애니메이션 적용

--- src/components/project/ProjectDetail.tsx
+++ src/components/project/ProjectDetail.tsx
@@ -1,3 +1,4 @@
+"use client";
 import { ProjectData } from "@/types/project";
 import ProjectBanner from "./ProjectBanner";
 import ProjectDescription from "./ProjectDescription";
@@ -7,12 +8,18 @@ import ProjectPeriod from "./ProjectPeriod";
 import ProjectStacks from "./ProjectStacks";
 import ProjectType from "./ProjectType";
 import BackButton from "./BackButton";
+import { motion } from "framer-motion";
 
 interface ProjectProps {
   project: ProjectData;
 }
 
 export default function ProjectDetail({ project }: ProjectProps) {
+  const fadeUpVariant = {
+    hidden: { opacity: 0, y: 60 },
+    visible: { opacity: 1, y: 0, transition: { duration: 0.8 } },
+  };
+
   return (
     <main className="w-full pb-30">
       <BackButton />
@@ -24,12 +31,59 @@ export default function ProjectDetail({ project }: ProjectProps) {
         gradient={project.gradient}
       />
       <div className="w-[90vw] max-w-[1232px] mx-auto space-y-10 mt-60 ">
-        <ProjectDescription description={project.description} />
-        <ProjectPeriod period={project.period} />
-        <ProjectType type={project.type} />
-        <ProjectLinks links={project.links} />
-        <ProjectStacks stacks={project.stacks} />
-        <ProjectFeatures id={project.id} />
+        <motion.div
+          variants={fadeUpVariant}
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true, amount: 0.5 }}
+        >
+          <ProjectDescription description={project.description} />
+        </motion.div>
+
+        <motion.div
+          variants={fadeUpVariant}
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true, amount: 0.5 }}
+        >
+          <ProjectPeriod period={project.period} />
+        </motion.div>
+
+        <motion.div
+          variants={fadeUpVariant}
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true, amount: 0.5 }}
+        >
+          <ProjectType type={project.type} />
+        </motion.div>
+
+        <motion.div
+          variants={fadeUpVariant}
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true, amount: 0.5 }}
+        >
+          <ProjectLinks links={project.links} />
+        </motion.div>
+
+        <motion.div
+          variants={fadeUpVariant}
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true, amount: 0.5 }}
+        >
+          <ProjectStacks stacks={project.stacks} />
+        </motion.div>
+
+        <motion.div
+          variants={fadeUpVariant}
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true, amount: 0.1 }}
+        >
+          <ProjectFeatures id={project.id} />
+        </motion.div>
       </div>
     </main>
   );
